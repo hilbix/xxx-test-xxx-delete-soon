360adf3b353ebc9b2431bd0de06343540825fcf3
Valentin Hilbig <webmaster@scylla-charybdis.com>
2013-02-14 21:17:58 +0100

    8

--- test.sh
+++ test.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-KEY=/dev/disk/by-id/usb-Generic_STORAGE_DEVICE_000000009451-0:0-part1
-LEN=512
-
-out()
-{
-echo "$*" >&2
-}
-
-out "Loading USB"
-modprobe usb_storage
-out "Waiting for USB to come up"
-sleep 10
-if [ -b "$KEY" ]
-then
-	out "Unlocking LUKS with $KEY"
-	exec dd if="$KEY" bs="$LEN" count=1 2>/dev/null
-fi
-
-out "Failed to detect $KEY"
-exec /lib/cryptsetup/askpass "LUKS password: "
+#KEY=/dev/disk/by-id/usb-Generic_STORAGE_DEVICE_000000009451-0:0-part1
+#LEN=512
+#
+#out()
+#{
+#echo "$*" >&2
+#}
+#
+#out "Loading USB"
+#modprobe usb_storage
+#out "Waiting for USB to come up"
+#sleep 10
+#if [ -b "$KEY" ]
+#then
+#	out "Unlocking LUKS with $KEY"
+#	exec dd if="$KEY" bs="$LEN" count=1 2>/dev/null
+#fi
+#
+#out "Failed to detect $KEY"
+#exec /lib/cryptsetup/askpass "LUKS password: "
